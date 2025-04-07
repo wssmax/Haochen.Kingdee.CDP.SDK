4811c517cd3f3ef9ec2bc6f82618c68d2408a557
Haochen <wssmax@163.com>
2025-04-07 19:49:52 +0800

    Init project

---
+++ src/Haochen.Kingdee.CDP.SDK/Base/KingdeeInput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haochen.Kingdee.CDP.Base;
+
+/// <summary>
+/// 金蝶api保存功能基类
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public abstract class KingDeeInput<T> where T : new()
+{
+    /// <summary>
+    /// 需要更新的字段，数组类型，格式：[key1,key2,...] （非必录）
+    /// 注（更新字段时Model数据包中必须设置内码，若更新单据体字段还需设置分录内码）
+    /// </summary>
+    public List<string> NeedUpDateFields { get; set; } = [];
+
+    /// <summary>
+    /// 需返回结果的字段集合，数组类型，格式：[key,entitykey.key,...]（非必录）
+    /// 注（返回单据体字段格式：entitykey.key）
+    /// </summary>
+    public List<string> NeedReturnFields { get; set; } = [];
+
+    /// <summary>
+    /// 表单数据包，JSON类型（必录）
+    /// </summary>
+    public T Model { get; set; } = new T();
+}
---
+++ src/Haochen.Kingdee.CDP.SDK/Base/KingdeeResponse.cs
@@ -0,0 +1,89 @@
+﻿namespace Haochen.Kingdee.CDP.SDK.Base;
+
+
+/// <summary>
+/// 表示整个响应结果,无返回值
+/// </summary>
+public class KingDeeResponse
+{
+    public ResponseResult Result { get; set; }
+}
+
+/// <summary>
+/// 表示整个响应结果，有多返回值
+/// </summary>
+public class MultiResponse<T>
+{
+    public ResponseResult<T> Result { get; set; }
+}
+/// <summary>
+/// 单返回值,单独查询结果
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class SingleResponse<T>
+{
+    public SingleResponseResult<T> Result { get; set; }
+}
+
+/// <summary>
+/// 表示响应的结果部分
+/// </summary>
+public class ResponseResult : ResponseBase
+{
+    public ResponseStatus ResponseStatus { get; set; }
+}
+
+/// <summary>
+/// 表示响应的结果部分
+/// </summary>
+public class ResponseResult<T> : ResponseResult
+{
+    public List<T> NeedReturnData { get; set; }
+}
+
+public class SingleResponseResult<T> : ResponseResult
+{
+    public T Result { get; set; }
+}
+
+/// <summary>
+/// 表示响应状态
+/// </summary>
+public class ResponseStatus
+{
+    public string ErrorCode { get; set; }
+    public bool IsSuccess { get; set; }
+    public List<KingDeeMessage> Errors { get; set; }
+    public List<SuccessEntity> SuccessEntitys { get; set; }
+    public List<KingDeeMessage> SuccessMessages { get; set; }
+    public string MsgCode { get; set; }
+}
+
+/// <summary>
+/// 表示错误信息
+/// </summary>
+public class KingDeeMessage
+{
+    public string FieldName { get; set; }
+    public string Message { get; set; }
+    public int DIndex { get; set; }
+
+    public override string ToString()
+    {
+        return Message;
+    }
+}
+
+/// <summary>
+/// 表示成功实体
+/// </summary>
+public class SuccessEntity : ResponseBase
+{
+    public int DIndex { get; set; }
+}
+
+public abstract class ResponseBase
+{
+    public string Id { get; set; }
+    public string Number { get; set; }
+}
